f7e4d9609abe0529f679ae201f64733c8a3f4ed4
magas1507 <asmg15.07@gmail.com>
2026-06-07 17:31:36 -0300

    feat: create custom interfaces and errors

---
+++ src/models/Pokemon.ts
@@ -0,0 +1,19 @@
+export interface PokemonResumo {
+  id: number;
+  nome: string;
+  tipos: string[];
+  altura: number;
+  peso: number;
+  hp: number;
+  ataque: number;
+  defesa: number;
+}
+
+export interface PokemonApiResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: { type: { name: string } }[];
+  stats: { base_stat: number; stat: { name: string } }[];
+}
